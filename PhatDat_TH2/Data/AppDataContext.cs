c317df7c2a5ad1554a5cbc11d9fdb0c200ee8373
nguyendat13 <dat48421@gmail.com>
2025-03-29 14:29:06 +0700

    first

---
+++ PhatDat_TH2/Data/AppDataContext.cs
@@ -0,0 +1,13 @@
+﻿using PhatDat_TH2.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace PhatDat_TH2.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
---
+++ PhatDat_TH2/Model/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace PhatDat_TH2.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Avatar { get; set; }  
+        //public int CategoryId { get; set; }
+
+    }
+}
