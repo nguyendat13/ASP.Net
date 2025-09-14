611a053f6afa7056bb6a75ccd38324abf05d0f41
nguyendat13 <dat48421@gmail.com>
2025-09-14 14:02:40 +0700

    update code

--- src/config.js
+++ src/config.js
@@ -1,6 +1,3 @@
-const API_BASE_URL =
-  window.location.hostname === "localhost"
-    ? "https://localhost:7177"
-    : "https://fruit-store-pb5n.onrender.com";
-
+// config.js
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:7177";
 export default API_BASE_URL;
